7af641f3107cd1616f56178b98c679b3c0acc6df
Noval <novalariandy@gmail.com>
2025-12-01 16:17:35 +0800

    prisma update for serverless architecture

--- services/service.js
+++ services/service.js
@@ -20,7 +20,16 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+// Serverless-friendly Prisma client singleton
+let prisma;
+if (process.env.NODE_ENV === 'production') {
+	prisma = new PrismaClient();
+} else {
+	if (!global.prisma) {
+		global.prisma = new PrismaClient();
+	}
+	prisma = global.prisma;
+}
 const JWT_SECRET = process.env.JWT_SECRET;
 
 async function signUp({ email, password, firstName, lastName }) {
